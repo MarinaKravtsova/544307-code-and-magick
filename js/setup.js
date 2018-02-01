14ed0a9f901de5f33e7548d81fa9a1d61f2e1447
Marina <kanariekanarie@rambler.ru>
2018-02-01 21:33:10 +0500

    предварительное исправление замечаний

--- js/setup.js
+++ js/setup.js
@@ -4,46 +4,34 @@ var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', '�
 var COAT = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var EYES = ['black', 'red', 'blue', 'yellow', 'green'];
 
-var removedWizardNames = [];
-var removedWizardSurnames = [];
-var removedCoat = [];
-var removedEyes = [];
-
-for (var x = 0; x < 4; x++) {
-  removedWizardNames[x] = WIZARD_NAMES.splice(Math.floor(Math.random() * WIZARD_NAMES.length), 1);
-  removedWizardSurnames[x] = WIZARD_SURNAMES.splice(Math.floor(Math.random() * WIZARD_NAMES.length), 1);
-  removedCoat[x] = COAT.splice(Math.floor(Math.random() * COAT.length), 1);
-  removedEyes[x] = EYES.splice(Math.floor(Math.random() * EYES.length), 1);
-}
+var wizardNames = WIZARD_NAMES;
+var wizardSurnames = WIZARD_SURNAMES;
+var coat = COAT;
+var eyes = EYES;
 
 var userDialog = document.querySelector('.setup');
-userDialog.classList.remove('hidden');
+var removeHidden = function (dom) {
+  dom.classList.remove('hidden');
+};
+removeHidden(userDialog);
+removeHidden(userDialog.querySelector('.setup-similar'));
 
 var similarListElement = userDialog.querySelector('.setup-similar-list');
 var similarWizardTemplate = document.querySelector('#similar-wizard-template').content;
 
-var wizards = [
-  {
-    name: removedWizardNames[0] + '   ' + removedWizardSurnames[0],
-    coatColor: removedCoat[0],
-    eyesColor: removedEyes[0]
-  },
-  {
-    name: removedWizardNames[1] + '   ' + removedWizardSurnames[1],
-    coatColor: removedCoat[1],
-    eyesColor: removedEyes[1]
-  },
-  {
-    name: removedWizardNames[2] + '   ' + removedWizardSurnames[2],
-    coatColor: removedCoat[2],
-    eyesColor: removedEyes[2]
-  },
-  {
-    name: removedWizardNames[3] + '   ' + removedWizardSurnames[3],
-    coatColor: removedCoat[3],
-    eyesColor: removedEyes[3]
-  }
-];
+var wizards = [];
+
+var wizardFunction = function () {
+  var wizard = {
+    name: wizardNames.splice(Math.floor(Math.random() * wizardNames.length), 1) + '   ' + wizardSurnames.splice(Math.floor(Math.random() * wizardSurnames.length), 1),
+    coatColor: coat.splice(Math.floor(Math.random() * coat.length), 1),
+    eyesColor: eyes.splice(Math.floor(Math.random() * eyes.length), 1)
+  };
+  return wizard;
+};
+for (var y = 0; y <= 3; y++) {
+  wizards[y] = wizardFunction();
+}
 
 var renderWizard = function (wizard) {
   var wizardElement = similarWizardTemplate.cloneNode(true);
@@ -60,5 +48,3 @@ for (var i = 0; i < wizards.length; i++) {
   fragment.appendChild(renderWizard(wizards[i]));
 }
 similarListElement.appendChild(fragment);
-
-userDialog.querySelector('.setup-similar').classList.remove('hidden');
