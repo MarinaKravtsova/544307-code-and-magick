14f19a9f6cf555e1c5c75027dc307cebb3a484af
Marina <kanariekanarie@rambler.ru>
2018-02-01 12:18:32 +0500

    цвета и имена не повторяются

--- js/setup.js
+++ js/setup.js
@@ -4,6 +4,18 @@ var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', '�
 var COAT = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var EYES = ['black', 'red', 'blue', 'yellow', 'green'];
 
+var removedWizardNames = [];
+var removedWizardSurnames = [];
+var removedCoat = [];
+var removedEyes = [];
+
+for (var x = 0; x < 4; x++) {
+  removedWizardNames[x] = WIZARD_NAMES.splice(Math.floor(Math.random() * WIZARD_NAMES.length), 1);
+  removedWizardSurnames[x] = WIZARD_SURNAMES.splice(Math.floor(Math.random() * WIZARD_NAMES.length), 1);
+  removedCoat[x] = COAT.splice(Math.floor(Math.random() * COAT.length), 1);
+  removedEyes[x] = EYES.splice(Math.floor(Math.random() * EYES.length), 1);
+}
+
 var userDialog = document.querySelector('.setup');
 userDialog.classList.remove('hidden');
 
@@ -12,24 +24,24 @@ var similarWizardTemplate = document.querySelector('#similar-wizard-template').c
 
 var wizards = [
   {
-    name: WIZARD_NAMES[Math.floor(Math.random() * WIZARD_NAMES.length)] + '   ' + WIZARD_SURNAMES[Math.floor(Math.random() * WIZARD_NAMES.length)],
-    coatColor: COAT[Math.floor(Math.random() * COAT.length)],
-    eyesColor: EYES[Math.floor(Math.random() * EYES.length)]
+    name: removedWizardNames[0] + '   ' + removedWizardSurnames[0],
+    coatColor: removedCoat[0],
+    eyesColor: removedEyes[0]
   },
   {
-    name: WIZARD_NAMES[Math.floor(Math.random() * WIZARD_NAMES.length)] + '   ' + WIZARD_SURNAMES[Math.floor(Math.random() * WIZARD_NAMES.length)],
-    coatColor: COAT[Math.floor(Math.random() * COAT.length)],
-    eyesColor: EYES[Math.floor(Math.random() * EYES.length)]
+    name: removedWizardNames[1] + '   ' + removedWizardSurnames[1],
+    coatColor: removedCoat[1],
+    eyesColor: removedEyes[1]
   },
   {
-    name: WIZARD_NAMES[Math.floor(Math.random() * WIZARD_NAMES.length)] + '   ' + WIZARD_SURNAMES[Math.floor(Math.random() * WIZARD_NAMES.length)],
-    coatColor: COAT[Math.floor(Math.random() * COAT.length)],
-    eyesColor: EYES[Math.floor(Math.random() * EYES.length)]
+    name: removedWizardNames[2] + '   ' + removedWizardSurnames[2],
+    coatColor: removedCoat[2],
+    eyesColor: removedEyes[2]
   },
   {
-    name: WIZARD_NAMES[Math.floor(Math.random() * WIZARD_NAMES.length)] + '   ' + WIZARD_SURNAMES[Math.floor(Math.random() * WIZARD_NAMES.length)],
-    coatColor: COAT[Math.floor(Math.random() * COAT.length)],
-    eyesColor: EYES[Math.floor(Math.random() * EYES.length)]
+    name: removedWizardNames[3] + '   ' + removedWizardSurnames[3],
+    coatColor: removedCoat[3],
+    eyesColor: removedEyes[3]
   }
 ];
 
